02206d6175e985280aafe7f2c8f3e2ee08acd319
zacharysmith51 <zacharysmith@mcps.org>
2020-11-09 18:58:44 +0000

    update game.java

--- Unit3_game/src/main/java/net/vtcpemba/hokiezack/CSA/Game.java
+++ Unit3_game/src/main/java/net/vtcpemba/hokiezack/CSA/Game.java
@@ -9,20 +9,21 @@ import java.util.Scanner;
 public class Game 
 {
     public int runs = 0;
-    public int wins = 0;
-    public int losses = 0;
+    public int pwins = 0;
+    public int cwins = 0;
+    public int ties = 0;
     Scanner scan = new Scanner(System.in);
     public  void run(){
         int x = 0;
         while (x < 20){
             int cmove = selectMove();
-            int pmove = 
+            int pmove = enterMove();
+            
             x++;
         }
     }
     int selectMove(){
         int RN = (int)(Math.random()*3);
-        System.out.println(RN);
         return RN;// 0 = rock, 1 = paper, 2 = Scissors
     }
     int enterMove(){
@@ -37,6 +38,30 @@ public class Game
         }else {
             System.out.println("not a valid anwser please choose another anwser");/*retry if invalid anwser*/
             return enterMove();
+        }
+    }
+    int checkWinner(int P1, int P2){
+        /*0 = tie, 1 = Computer win, 2 = Player win, 4 = error*/
+        if (cmove == 0 && pmove == 0){
+            return 0; 
+        } else if (cmove == 0 && pmove == 1){
+            return 3; 
+        } else if (cmove == 0 && pmove == 2){
+            return 2; 
+        } else if (cmove == 1 && pmove == 0){
+            return 2; 
+        } else if (cmove == 1 && pmove == 1){
+            return 0; 
+        } else if (cmove == 1 && pmove == 2){
+            return 3; 
+        } else if (cmove == 2 && pmove == 0){
+            return 3; 
+        } else if (cmove == 2 && pmove == 1){
+            return 2; 
+        } else if (cmove == 2 && pmove == 2){
+            return 0; 
+        } else {
+            return 4;
         }
     }
 }
