25a4c125a6923b61f07c1685b8b3c3d436ebd9d7
Zachary Zachary <zacharysmith@mcps.org>
2020-11-16 17:19:38 +0000

    fix exit code

--- Unit3_game/src/main/java/net/vtcpemba/hokiezack/CSA/Cli.java
+++ Unit3_game/src/main/java/net/vtcpemba/hokiezack/CSA/Cli.java
@@ -5,20 +5,19 @@ public class Cli {
   App app = new App();
   public  void run(){
     int x = 0;
-      while (x < 3){
-        int cmove = app.game.selectMove();
-        int pmove = app.game.enterMove();
-        app.game.updateStats(app.game.checkWinner(cmove, pmove));
-        System.out.println("do you want to play again?");            String play = App.scan.nextLine();
-        play = App.scan.nextLine();
-        if (play.equals("yes") || play.equals("Yes")){
-          System.out.print("ok");
-          x=0;
-        } else if (play == "No" || play == "no") {
-          System.out.print(app.game.runs+" "+app.game.pwins+" "+app.game.cwins+" "+app.game.ties);
-          break;
-        }
-        
-      }
+    while (x < 3){
+      int cmove = app.game.selectMove();
+      int pmove = app.game.enterMove();
+      app.game.updateStats(app.game.checkWinner(cmove, pmove));
+      System.out.println("do you want to play again?");            String play = App.scan.nextLine();
+      play = App.scan.nextLine();
+      if (play.equals("yes") || play.equals("Yes")){
+        System.out.print("ok");
+        x=0;
+      } else if (play.equals("No") || play.equals("no") {
+        System.out.print("you have played " + app.game.runs + " times. you have won "+app.game.pwins+" times. you have lost "+app.game.cwins+" times. you have tied "+app.game.ties+" times.");
+        break;
+      }        
     }
+  }
 }
